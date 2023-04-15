8eabe116b07cb937170f37e4cc08e4ab63f526d9
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-15 19:43:40 +0200

    Add method to map faire item to baselinker product.

--- MoveOrders/MoveOrders.cs
+++ MoveOrders/MoveOrders.cs
@@ -14,10 +14,8 @@ namespace MoveOrders
     public class MoveOrders
     {
         [FunctionName("MoveOrders")]
-        public void Run([TimerTrigger("0 */10 * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */10 * * * *")] TimerInfo myTimer, ILogger log)
         {
-            // log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
             var faireAccessToken = Environment.GetEnvironmentVariable("X-FAIRE-ACCESS-TOKEN");
             var baseLinkerToken = Environment.GetEnvironmentVariable("X-BLToken");
 
@@ -51,22 +49,27 @@ namespace MoveOrders
 
 
         /* Dokumentacja Baselinkera informuje o przepustowości ograniczonej do 100 zapytań na minutę. Może nastąpić sytuacja, że jednorazowo będziemy chcieli dodać więcej niż 100 zamówień. 
-           Cześciowo rozwiązałem ten problem z pomocą klasy TimerInfo, ponieważ po pierwszym wykonaniu funkcji będa pobierane jedynie zamówienia sprzed ostatnich 10 min, więc jest mała szansa na wykorzystanie limitu.
+           Cześciowo rozwiązałem ten problem z pomocą klasy TimerInfo, ponieważ po pierwszym wykonaniu funkcji będa pobierane jedynie zamówienia sprzed ostatnich 10 min, więc jest mała szansa na przekroczenie limitu.
            Wszystko zależy od tego jak duży jest ruch w sklepie, z którego pobieramy zamówienia.   
            W celu całkowitej pewności, że problem ten nie wystąpi, widziałbym kilka możliwych rozwiązań.
 
            Pierwszą i najbardziej prymitywną opcją jest stworzenie w poniższej funkcji licznika w pętli, który po osiągnięciu danej liczby
              (bezpieczniejszą wartościa od 100 była by jakaś liczba około 90, bo w ciągu danej minuty mogły być tez wykonane już przez nas jakieś inne operacje) zatrzyma wykonywanie programu na minutę.
 
-           Drugą opcją było by wymuszanie opóźnienia bezpośrednio pomiędzy przesyłaniem zapytań np. wymusić konieczność wysyłania jednego zapytania na sekunde. 
+           Drugą opcją było by wymuszanie opóźnienia bezpośrednio pomiędzy przesyłaniem zapytań np. wymusić konieczność wysyłania maksymalnie jednego zapytania na sekunde. 
 
-           Kolejną możliwością byłoby skorzystanie z jakiejś bramki API do przekierowywania ruchu, która współpracuje z baselinkerem. */
+           Kolejną możliwością byłoby skorzystanie z jakiejś bramki API do przekierowywania ruchu, która współpracuje z Baselinkerem.
+        
+           Z tego co się również dowiedziałem istnieje możliwość negocjacji maksymalnego limitu zapytan z Baselinkerem.
+        
+           Uważam że nie ma tu jednego, najlepszego rozwiązania. Wszystko zależy od potrzeb, ilości zasóbów oraz tego jak duży jest ruch w sklepie.
+           W związku z tym, że nic nie było o tym wspomniane w poleceniu zadania, założyłem że ruch jest na tyle mały, iż moje obecne rozwiązanie problemu wystarczy na ten moment. */
 
         private static void AddBaseLinkerOrders(List<BaseLinkerOrder> orders, string baselinkerToken)
         {
             var client = new RestClient("https://api.baselinker.com/connector.php");
 
-            foreach (var baseLinkerOrder in orders) // W dokumentacji BaseLinkera nie ma metody, ktora pozwolilaby na jednorazowe dodanie wszystkich zamowien, wiec dodaje po jednym.
+            foreach (var baseLinkerOrder in orders) // W dokumentacji BaseLinkera, do której mam dostęp nie ma metody, która pozwolilaby na jednorazowe dodanie wszystkich zamowien, wiec dodaje po jednym.
             {
 
                 var request = new RestRequest("https://api.baselinker.com/connector.php", Method.Post);
@@ -79,7 +82,7 @@ namespace MoveOrders
             }
         }
 
-       private static List<BaseLinkerOrder> ConvertFaireToBaseLinkerOrder(List<FaireOrder> faireOrders)
+        private static List<BaseLinkerOrder> ConvertFaireToBaseLinkerOrder(List<FaireOrder> faireOrders)
         {
             var baseLinkerOrders = new List<BaseLinkerOrder>();
 
@@ -89,14 +92,37 @@ namespace MoveOrders
                 {
                     OrderStatusId = 8069,
                     CustomSourceId = 1024,
-                    DateAdd = (int)((DateTimeOffset)faireOrder.CreatedAt).ToUnixTimeSeconds(),                   
+                    DateAdd = (int)((DateTimeOffset)faireOrder.CreatedAt).ToUnixTimeSeconds(),
+                    Phone = faireOrder.Address.PhoneNumber,
+                    DeliveryAddress = faireOrder.Address.Address1 + "\n" + faireOrder.Address.Address2,
                     ExtraField1 = faireOrder.Id,
+                    Products = MapFaireItemsToBaselinkerProducts(faireOrder.Items),
                 };
                 baseLinkerOrders.Add(convertedBaseLinkerOrder);
             }
 
             return baseLinkerOrders;
         }
+
+
+        private static List<Product> MapFaireItemsToBaselinkerProducts(List<FaireOrderItem> items)
+        {
+            var baselinkerProducts = new List<Product>();
+
+            foreach (var item in items)
+            {
+
+                var ConvertedProduct = new Product()
+                {
+                    ProductId = item.ProductId,
+                    Name = item.ProductName,
+                    Quantity = item.Quantity,
+                };
+
+                baselinkerProducts.Add(ConvertedProduct);
+            }
+
+            return baselinkerProducts;
+        }
     }
 }
-
